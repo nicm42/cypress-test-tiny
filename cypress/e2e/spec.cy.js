724a65555109513630773c3d92636f8856d7922e
“nicm42” <42087659+nicm42@users.noreply.github.com>
2023-12-21 20:48:57 +0000

    Added failing test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,8 @@
 /// <reference types="cypress" />
 describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+  it('should say back of card is not visible', () => {
+    cy.visit('index.html');
+    cy.get('.card-front').should('be.visible');
+    cy.get('.card-back').should('not.be.visible');
+  });
+});
